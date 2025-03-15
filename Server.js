1803bedc48b2381befd607320512a96c308da85d
Nantipat-va <nantipat.va@gmail.com>
2025-03-15 23:48:16 +0700

    Read csv and show on ejs

--- Server.js
+++ Server.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const http = require('http');
+const fs = require('fs');
+const csv = require('csv-parser')
 
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/pubilc'));
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/views/companiesList.html');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+
+//Convert CSV File to Object
+var objects = [];
+
+fs.createReadStream('./listedCompanies_th_TH.csv')
+  .pipe(
+    csv()
+  )
+  .on('data', (data) => objects.push(Object.values(data)))
+  .on('end', () => {
+    console.log(objects)
+  })
+
+app.get('/',(req, res) =>{
+  res.render('companiesList', {data: objects})
 })
 
 server.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 })
